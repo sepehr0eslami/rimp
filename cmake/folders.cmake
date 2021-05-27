d60012a28249ee732888cfaabc02bbe768167d27
Sepehr Eslami <sepehr0eslami@gmail.com>
2021-05-27 15:52:49 +0430

    📈Variablized important folders using CMake variables new file:   cmake/folders.cmake

---
+++ cmake/folders.cmake
@@ -0,0 +1,8 @@
+# Variable for the folder containing the actuall source files.
+set(source_dir "${PROJECT_SOURCE_DIR}/src")
+
+# Variable for the folder containing CMake/Project related files.
+set(cmake_dir "${PROJECT_SOURCE_DIR}/cmake")
+
+# Variable for the folder containing scripts.
+set(scripts_dir "${PROJECT_SOURCE_DIR}/scripts")
