791225288fa014ddfa4170a319b3406f0a84b298
Sepehr Eslami <sepehr0eslami@gmail.com>
2021-05-27 17:39:44 +0430

    ↪Moved ExternalProjects to the new directory "external" Added the "external" directory to the .gitignore file

--- cmake/Modules/CLI11.cmake
+++ cmake/Modules/CLI11.cmake
@@ -11,9 +11,13 @@ FetchContent_Declare(CLI11
     GIT_REPOSITORY  https://github.com/CLIUtils/CLI11.git
     GIT_TAG         master
     GIT_SHALLOW     True
+
+    SOURCE_DIR      "${PROJECT_SOURCE_DIR}/external/CLI11"
     )
 
 # Clone the "CLI11" repository.
 FetchContent_Populate(CLI11)
 
 # CMake by default will put this reposity in the "Rimp/build/_deps/" directory.
+# But since we changed the value of SOURCE_DIR, now it will clone it in
+# "Rimp/external/CLI11".
