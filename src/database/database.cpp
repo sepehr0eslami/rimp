9c84f5d8f9fc70975b10ec48efd4fb938e599e96
Sepehr Eslami <sepehr0eslami@gmail.com>
2021-06-14 17:29:45 +0430

    ➕Implemented the "SQLDatabase" Class

---
+++ src/database/database.cpp
@@ -0,0 +1,41 @@
+/** 
+ * Rimp(Reimplementer). Simple and fast File Tagger.
+ * 
+ * Copyright (C) 2021 Sepehr Eslami <sepehr0eslami@gmail.com>
+ * 
+ * This file is a part of Rimp.
+ *
+ * Rimp is free software: you can  redistribute  it  and/or modify  it under the
+ * terms of  the  GNU General Public License  as  published by the Free Software
+ * Foundation, either  version 3 of  the  License, or (at your option) any later
+ * version.
+ *
+ * Rimp  is  distributed  in  the hope that  it  will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See  the GNU General Public License  for  more details.
+ *
+ * You should have received  a copy of the GNU General Public License along with
+ * Rimp. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "src/database/database.h"
+
+SQLDatabase::SQLDatabase(filesystem::path database_path)
+    : database_file_(database_path) {
+    int returned = 0;
+    returned = sqlite3_open(database_file_.c_str(), &sqlite_object_);
+    if (returned) {
+        string error_msg = sqlite3_errmsg(sqlite_object_);
+        error_msg.append(": " + database_file_.string() + "\n");
+        error_msg.append(
+            "Please make sure the file and Its parent directory are accessible "
+            "for you(You have Read/Write access to both); and if the issue "
+            "didn't resolve, Please file a bug report at:\n"
+            "https://github.com/sepehr0eslami/rimp/issues\n");
+        throw runtime_error{error_msg};
+    }
+}
+
+SQLDatabase::~SQLDatabase() {
+    sqlite3_close(sqlite_object_);
+}
---
+++ src/database/database.h
@@ -0,0 +1,42 @@
+/** 
+ * Rimp(Reimplementer). Simple and fast File Tagger.
+ * 
+ * Copyright (C) 2021 Sepehr Eslami <sepehr0eslami@gmail.com>
+ * 
+ * This file is a part of Rimp.
+ *
+ * Rimp is free software: you can  redistribute  it  and/or modify  it under the
+ * terms of  the  GNU General Public License  as  published by the Free Software
+ * Foundation, either  version 3 of  the  License, or (at your option) any later
+ * version.
+ *
+ * Rimp  is  distributed  in  the hope that  it  will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See  the GNU General Public License  for  more details.
+ *
+ * You should have received  a copy of the GNU General Public License along with
+ * Rimp. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SRC_DATABASE_DATABASE_H_
+#define SRC_DATABASE_DATABASE_H_
+
+#include <sqlite3.h>
+
+#include <string>
+
+#include "src/paths/filesystem.h"
+
+using namespace std;  // NOLINT
+
+class SQLDatabase {
+ public:
+    explicit SQLDatabase(filesystem::path database_file);
+    ~SQLDatabase();
+
+ private:
+    filesystem::path database_file_;
+    sqlite3 *sqlite_object_;
+};
+
+#endif  // SRC_DATABASE_DATABASE_H_
