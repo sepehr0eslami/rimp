fb9d0a53e2f4274ab8cec0f01669b7721bd04100
Sepehr Eslami <sepehr0eslami@gmail.com>
2021-06-14 14:20:19 +0430

    ➕Implemented the "SQLTable" Class

---
+++ src/database/table.cpp
@@ -0,0 +1,35 @@
+/** 
+ * Rimp(Reimplementer). Simple and fast File Tagger.
+ * 
+ * Copyright (C) 2021 Sepehr Eslami <sepehr0eslami@gmail.com>
+ * 
+ * This file is a part of Rimp.
+ *
+ * Rimp is free software: you can  redistribute  it  and/or modify  it under the
+ * terms of  the  GNU General Public License  as  published by the Free Software
+ * Foundation, either  version 3 of  the  License, or (at your option) any later
+ * version.
+ *
+ * Rimp  is  distributed  in  the hope that  it  will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See  the GNU General Public License  for  more details.
+ *
+ * You should have received  a copy of the GNU General Public License along with
+ * Rimp. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "src/database/table.h"
+
+SQLTable::SQLTable(string table_name, vector<string> table_columns,
+                   vector<SQLDataTypes> columns_type) {
+    if (table_columns.size() != columns_type.size()) {
+        throw runtime_error{
+            "Error while creating an instance of \"SQLTable\": Given columns "
+            "don't match the given types.\nPlease file a bug report at:\n"
+            "https://github.com/sepehr0eslami/rimp"};
+    }
+
+    name_ = table_name;
+    columns_ = table_columns;
+    types_ = columns_type;
+}
---
+++ src/database/table.h
@@ -0,0 +1,47 @@
+/** 
+ * Rimp(Reimplementer). Simple and fast File Tagger.
+ * 
+ * Copyright (C) 2021 Sepehr Eslami <sepehr0eslami@gmail.com>
+ * 
+ * This file is a part of Rimp.
+ *
+ * Rimp is free software: you can  redistribute  it  and/or modify  it under the
+ * terms of  the  GNU General Public License  as  published by the Free Software
+ * Foundation, either  version 3 of  the  License, or (at your option) any later
+ * version.
+ *
+ * Rimp  is  distributed  in  the hope that  it  will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See  the GNU General Public License  for  more details.
+ *
+ * You should have received  a copy of the GNU General Public License along with
+ * Rimp. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SRC_DATABASE_TABLE_H_
+#define SRC_DATABASE_TABLE_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;  // NOLINT
+
+enum SQLDataTypes {
+    Integral,
+    Text,
+    Blob
+};
+
+class SQLTable {
+ public:
+    SQLTable(string table_name, vector<string> table_columns,
+             vector<SQLDataTypes> columns_type);
+
+ private:
+    string name_;
+    vector<string> columns_;
+    vector<SQLDataTypes> types_;
+};
+
+#endif  // SRC_DATABASE_TABLE_H_
